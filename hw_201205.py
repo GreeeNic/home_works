96da8002f942398efce707297a08693c3f43399e
Никита <mr.gonk@mail.ru>
2020-12-09 20:56:33 +0500

    initial

--- hw_201205.py
+++ hw_201205.py
@@ -4,4 +4,11 @@ laptops = [
     ['Hp', '254$', '367$', '567$', '890$'],
     ['Honor', '299$', '345$', '587$', '6237$']
 ]
-def show_price()
+
+
+def show_price(laptops_price):
+    print(laptops_price[0], laptops_price[1:])
+
+
+for price in laptops:
+    show_price(price)
